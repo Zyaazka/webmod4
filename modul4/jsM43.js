62166d993ef57d5f0ad55db58d0dae1bce45560a
Zyaazka <zyaulazka@gmail.com>
2024-12-14 18:59:28 +0700

    Add files via upload

---
+++ modul4/jsM43.js
@@ -0,0 +1,12 @@
+function pesan() {
+    alert("Ini latihan javaScript Pertama saya");
+}
+
+function pesanConsole() {
+    console.log("it's work");
+}
+
+function pesanDok() {
+    document.write("Succces <br>");
+    document.write("<a href='m4-2.html'>Kembali</a>")
+}
